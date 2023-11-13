d50e03a1b1648bd70bd836648f1ce91c824a7249
Felipe Soares <felipe.11.11@hotmail.com>
2023-11-13 14:58:10 -0400

    Feat: Add responsive design in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-between items-center border-t border-lines">
-      <div className="flex px-6 gap-4 items-center">
+    <footer className="flex phone-md:justify-between items-center border-t border-lines">
+      <div className="flex pl-6 gap-4 items-center">
         <p>find me in</p>
         <a
           href="https://www.linkedin.com/in/felipe0848/"
@@ -19,7 +19,7 @@ export default function Footer() {
         className="hover:text-gray-300"
       >
         <div className="flex px-6 py-4 border-l border-lines gap-2 items-center">
-          <p>@felipe0848</p>
+          <p className="hidden phone-md:flex">@felipe0848</p>
           <i className="ri-github-fill ri-xl" />
         </div>
       </a>
