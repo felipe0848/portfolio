a6be07d0fd2cb3ede9dc9f9df48e945c3f3a6851
Felipe Soares <felipe.11.11@hotmail.com>
2023-11-14 11:32:07 -0400

    Refactor: responsive snippets code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
+'use client'
 import Image from 'next/image'
 import blursImg from '@/assets/background-blurs.svg'
 import SwiperSnippets from '@/components/home/swiper-snippets'
 import Welcome from '@/components/home/welcome'
+import { useMediaQuery } from 'react-responsive'
 
 export default function Home() {
+  const screenXl = useMediaQuery({ query: '(min-width:1280px)' })
   return (
     <div className="flex flex-col gap-20 h-full w-full justify-center items-center xl:flex-row p-8">
       <Welcome />
 
-      <div className="hidden xl:h-[700px] xl:flex ">
-        <SwiperSnippets direction="vertical" />
-      </div>
-      <div className="hidden sm:max-xl:w-full sm:max-xl:flex ">
-        <SwiperSnippets direction="horizontal" />
+      <div className="hidden xl:h-[700px] sm:w-full sm:max-w-[700px] sm:flex ">
+        <SwiperSnippets direction={screenXl ? 'vertical' : 'horizontal'} />
       </div>
 
       <Image
