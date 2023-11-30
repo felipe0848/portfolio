30b20fdbb5ea6bde0b7a599834620f630c745116
Felipe Soares <felipe.11.11@hotmail.com>
2023-11-30 17:10:33 -0400

    Fix: overflow projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
       <div className="border-y border-lines md:border-t-0">
         <NavigationTab />
       </div>
-      <div className="flex max-h-[80vh] grow flex-col flex-wrap items-center justify-start gap-x-6 gap-y-8 overflow-y-auto overflow-x-hidden p-8 max-[445px]:flex-nowrap md:max-h-[100vh] md:overflow-auto">
+      <div className="flex max-h-[80vh] grow flex-col flex-wrap items-center justify-start gap-x-6 gap-y-8 overflow-auto p-8 max-[445px]:flex-nowrap md:max-h-[100vh]">
         {projects.length > 0
           ? projects.map((project) => (
               <ProjectCard project={project} key={project.title} />
