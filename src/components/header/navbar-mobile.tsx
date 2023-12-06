6258f2a56032777d5ae748cfb30a10c06e281bc9
Felipe Soares <felipe.11.11@hotmail.com>
2023-12-06 14:05:57 -0400

    Feat: add Light Mode to navbar-mobile

--- src/components/header/navbar-mobile.tsx
+++ src/components/header/navbar-mobile.tsx
@@ -10,13 +10,13 @@ export default function NavbarMobile() {
         </button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 z-20 bg-primary-black" />
-        <Dialog.Content className="fixed inset-5 z-30 flex flex-col rounded-lg border border-lines bg-primary-blue">
+        <Dialog.Overlay className="fixed inset-0 z-20 bg-slate-100 dark:bg-primary-black" />
+        <Dialog.Content className="fixed inset-5 z-30 flex flex-col rounded-lg border border-lines bg-slate-300 dark:bg-primary-blue">
           <Dialog.Title className="w-full border-b border-lines p-4">
             <p>felipe-soares</p>
           </Dialog.Title>
           <Dialog.Description className="flex h-full flex-col justify-between">
-            <nav className="flex flex-col text-white">
+            <nav className="flex flex-col dark:text-gray-300">
               <Dialog.Close asChild>
                 <Link href="/" className={'border-b border-lines p-4'}>
                   _ola
--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,9 +1,20 @@
 'use client'
 
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 export default function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
   return (
     <div>
       {theme === 'light' ? (
